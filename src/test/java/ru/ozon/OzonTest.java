1ae747358a88c949c1ba8a3e7fa41e59e677d585
Ruslan <ruslan.gindullin@inbox.ru>
2022-05-07 20:46:41 +0500

    Доработка ДЗ

--- src/test/java/ru/ozon/OzonTest.java
+++ src/test/java/ru/ozon/OzonTest.java
@@ -10,25 +10,21 @@ import static org.junit.Assert.assertThat;
 
 public class OzonTest {
 
-//    @Before
-//    public void init(){
-//        ChromeDriver driver = new ChromeDriver();
-//    }
+    @Before
+    public void init(){
+        ChromeDriver driver;
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\79122\\Downloads\\chromedriver.exe");
+    }
 
-    ChromeDriver driver;
-
-
-    private String searchButton = "#stickyHeader > div.ch0 > div > form > button > svg";
-    private String addButton = "#layoutPage > div.gt > div.container.tg0 > div:nth-child(2) > div:nth-child(2) > div.ri3 > div:nth-child(1) > div > div > div:nth-child(1) > div.p4i > div.io7.i8o > div > div > div > button > span > span";
+    private String searchButton = "#stickyHeader > div.g8c > div > form > button > svg";
+    private String addButton = "#layoutPage > div.u1g > div.container.u2g > div:nth-child(2) > div:nth-child(2) > div.s5i > div:nth-child(1) > div > div > div:nth-child(1) > div.iq7 > div.i9p.iq0 > div > div > div > button > span > span";
     private String resource = "https://www.ozon.ru/";
-    private String deleteButton = "#layoutPage > div.gt > div > div > div.container.tg0 > div:nth-child(4) > div.tg6.gt4 > div:nth-child(1) > div > div.p3a > div:nth-child(2) > div.a9n > div > div > div > div.ao7.o7a > div > div.a8o > div > div:nth-child(2) > button > span > span";
+    private String deleteButton = "#layoutPage > div.u1g > div > div > div.container.u2g > div:nth-child(4) > div.u8g.g6u > div:nth-child(1) > div > div.p3a > div:nth-child(2) > div.a9n > div > div > div > div.ao7.o7a > div > div.a8o > div > div:nth-child(2) > button > span > span";
     private String yesButton = "body > div.vue-portal-target > div > div.ui-i3 > div > div > section > div.am7.undefined > div > button > span > span";
-    private String deleteButton2 = "#layoutPage > div.gt > div > div > div.container.tg0 > div:nth-child(4) > div.tg6.gt4 > div:nth-child(1) > div > div.p3a > div.an9 > div.a9n > div > div > div > div.ao7.o7a > div > div.a8o > div > div:nth-child(2) > button > span > span";
-
-    private String closeButton = "#layoutPage > div.gt > div > div > div:nth-child(3) > div > div > div > div.ui-i3 > div > div > div > div > div.c1c > div > button > span > svg > path";
+    private String deleteButton2 = "#layoutPage > div.u1g > div > div > div.container.u2g > div:nth-child(4) > div.u8g.g6u > div:nth-child(1) > div > div.p3a > div:nth-child(3) > div.a9n > div > div > div > div.ao7.o7a > div > div.a8o > div > div:nth-child(2) > button > span > span";
+    private String closeButton = "#layoutPage > div.u1g > div > div > div:nth-child(3) > div > div > div > div.ui-i3 > div > div > div > div > div.b9z > div > button > span > svg > path";
     @Test
     public void addProducts(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\79122\\Downloads\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get(resource);
         driver.manage().window().setSize(new Dimension(1400, 800));  //устанавливаем размер экрана побольше
@@ -46,33 +42,25 @@ public class OzonTest {
         driver.findElement(By.cssSelector(closeButton)).click();  //закрываем всплывающий баннер
         driver.get("https://www.ozon.ru/cart");
 
-        try{
-            assertThat(driver.findElement(By.cssSelector(".g3s:nth-child(4) > .tsCaptionBold")).getText(), is("2"));  //выполняем проверку, на то, что в корзине 2 товара
-        }catch (Exception e){
-            e.printStackTrace();  //если селектор на странице не найдётся, выбрасываем исключение и идёи дальше
-            System.out.println("selector not found");
-        }finally {
-            driver.findElement(By.cssSelector(deleteButton)).click();
-            driver.findElement(By.cssSelector(yesButton)).click();
+        assertThat(driver.findElement(By.cssSelector("#stickyHeader > div.cg9 > a:nth-child(4) > span.tsCaptionBold.c0k")).getText(), is("2"));  //выполняем проверку, на то, что в корзине 2 товара
+        driver.findElement(By.cssSelector(deleteButton)).click();
+        driver.findElement(By.cssSelector(yesButton)).click();
 
-            driver.findElement(By.cssSelector(deleteButton2)).click();
-            driver.findElement(By.cssSelector(yesButton)).click();
-            driver.get("https://www.ozon.ru/cart");
+        driver.findElement(By.cssSelector(deleteButton2)).click();
+        driver.findElement(By.cssSelector(yesButton)).click();
+        driver.get("https://www.ozon.ru/cart");
+
+        assertThat(driver.findElement(By.cssSelector("#stickyHeader > div.cg9 > a:nth-child(4) > span.tsCaptionBold.c0k")).getText(), is("0")); //выполняем проверку, на то, что в корзине нет товаров
+        driver.quit();
         }
 
-        try{
-            assertThat(driver.findElement(By.cssSelector(".g3s:nth-child(4) > .tsCaptionBold")).getText(), is("0")); //выполняем проверку, на то, что в корзине нет товаров
-        }catch (Exception e){
-            e.printStackTrace();  //если селектор на странице не найдётся, выбрасываем исключение и идёи дальше
-            System.out.println("selector not found");
-        }finally {
-            driver.quit();
-        }
+
+    @After
+    public void close() {
+        ChromeDriver driver = new ChromeDriver();
+        driver.quit();
     }
-
-//    @After
-//    public void close(){
-//        driver.quit();
-//    }
-
 }
+
+
+
